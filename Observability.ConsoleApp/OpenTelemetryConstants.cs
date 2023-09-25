ac505cde42b88b4f2e7ebeaad465be0291ce5954
leventozz <leventufukozturk@gmail.com>
2023-09-25 22:04:05 +0300

    added opentelemetry paramaters

---
+++ Observability.ConsoleApp/OpenTelemetryConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Observability.ConsoleApp
+{
+    internal class OpenTelemetryConstants
+    {
+        //const string ServiceName = "Company.AppName.ComponentName";
+        internal const string ServiceName = "ConsoleApp";
+        internal const string ServiceVersion = "1.0.0";
+        internal const string ActivitySourceName = "ActivitySource.ConsoleApp";
+    }
+}
