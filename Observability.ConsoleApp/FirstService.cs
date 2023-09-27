be8197ec5b2931105308503ac778df5ce23f98ff
leventozz <leventufukozturk@gmail.com>
2023-09-27 10:58:14 +0300

    added tags and events

--- Observability.ConsoleApp/FirstService.cs
+++ Observability.ConsoleApp/FirstService.cs
@@ -1,13 +1,21 @@
-﻿namespace Observability.ConsoleApp
+﻿using System.Diagnostics;
+
+namespace Observability.ConsoleApp
 {
     internal class FirstService
     {
         static HttpClient httpClient = new();
         internal async Task<int> GetGoogleBytes()
         {
-            using var activity = ActivitySourceProvider.Source.StartActivity();
+            using var activity = ActivitySourceProvider.Source.StartActivity(kind: ActivityKind.Producer, name:"CustomGetGoogleBytes");
+
+            var eventTags = new ActivityTagsCollection();
+
+            activity?.AddEvent(new("Google Request Started"));
             var result = await httpClient.GetAsync("https://www.google.com");
             var responseContent = await result.Content.ReadAsStringAsync();
+            eventTags.Add("Google Body Lenght", responseContent.Length);
+            activity?.AddEvent(new("Google Request Finished", tags: eventTags));
             return responseContent.Length;
         }
     }
