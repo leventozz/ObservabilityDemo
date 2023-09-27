26f9226137e9ac7b19ecdc32f76e9a2cf0a8ba7c
leventozz <leventufukozturk@gmail.com>
2023-09-27 20:58:30 +0300

    added activity configuration

---
+++ Order.API/OpenTelemetry/ActivitySourceProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+
+namespace Observability.Order.API.OpenTelemetry
+{
+    internal static class ActivitySourceProvider
+    {
+        internal static ActivitySource Source = new ActivitySource();
+    }
+}
--- Order.API/Program.cs
+++ Order.API/Program.cs
@@ -1,3 +1,7 @@
+using Observability.Order.API.OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<OpenTelemetryConstants>(builder.Configuration.GetSection("OpenTelemetry"));
+
+
+
+builder.Services.AddOpenTelemetry().WithTracing(options =>
+{
+    var OTConstants = (builder.Configuration.GetSection("OpenTelemetry").Get<OpenTelemetryConstants>())!;
+    options.AddSource(OTConstants.ActivitySourceName)
+    .ConfigureResource(resource =>
+    {
+        resource.AddService(OTConstants.ServiceName, serviceVersion: OTConstants.ServiceVersion);
+    });
+    options.AddAspNetCoreInstrumentation();
+    options.AddConsoleExporter();
+    options.AddOtlpExporter();
+});
 
 var app = builder.Build();
 
