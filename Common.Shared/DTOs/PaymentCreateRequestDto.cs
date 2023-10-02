8677f799050a7e7a7b4125200f49b0f1ae2653fb
leventozz <leventufukozturk@gmail.com>
2023-10-02 14:36:39 +0300

    added payment api

---
+++ Common.Shared/DTOs/PaymentCreateRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.Shared.DTOs
+{
+    public record PaymentCreateRequestDto
+    {
+        public string OrderCode { get; set; } = null!;
+        public decimal TotalPrice { get; set; }
+    }
+}
---
+++ Payment.API/Controllers/PaymentController.cs
@@ -0,0 +1,22 @@
+﻿using Common.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Payment.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create(PaymentCreateRequestDto request)
+        {
+            const decimal balance = 1000;
+
+            if(request.TotalPrice > balance)
+                return BadRequest(ResponseDto<string>.Fail(HttpStatusCode.BadRequest.GetHashCode(), "Insufficient balance!"));
+
+            return Ok(ResponseDto<string>.Success(HttpStatusCode.OK.GetHashCode(), "Payment completed!"));
+        }
+    }
+}
